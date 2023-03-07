2ab52e1fd171b263078bd3be28952d6107a814c8
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-03-07 12:00:06 -0500

    Add files via upload

---
+++ Book.cpp
@@ -0,0 +1,118 @@
+/*
+Name: Eric Wu
+Professor: Tizana Ligorio
+Description: Added Operator Overloading to Book
+Course: CSCI 235 Spring 2023
+Due Date: 03/10/2023
+*/
+
+#include "Book.hpp"
+
+
+
+/**
+  Parameterized constructor.
+  @param      : The title of the book (a string)
+  @param      : The author of the book (a string)
+  @param      : The number of pages in the book (a positive int)
+  @param      : A flag indicating whether the book is in digital form (a Boolean),
+                with default value False
+  @post       : The private members are set to the values of the corresponding parameters.
+*/
+Book::Book(std::string title, std::string author, int page_count, bool is_digital):
+						title_{title}, author_{author}, page_count_{page_count}, is_digital_{is_digital}{}
+
+/**
+   @return : the title of the Book
+*/
+std::string Book::getTitle() const{
+	return title_;
+}
+
+/**
+  @return : the author of the Book
+*/
+std::string Book::getAuthor() const{
+	return author_;
+}
+
+/**
+  @return : the page count
+*/
+int Book::getPageCount() const{
+  return page_count_;
+}
+
+/**
+  @return : true if the book is available in digital form, false otherwise
+
+  Note: this is an accessor function and must follow the same convention as all accessor functions even if it is not called getDigital
+*/
+bool Book::isDigital() const{
+	return is_digital_;
+}
+
+/**
+  @param  : a reference to title of the Book
+  @post   : sets the Book's title to the value of the parameter
+*/
+void Book::setTitle(const std::string &title){
+	title_ = title;
+}
+
+/**
+  @param  : a reference to the name of the author of the Book
+  @post   : sets the Book's author to the value of the parameter
+*/
+void Book::setAuthor(const std::string &author){
+	author_ = author;
+}
+
+/**
+  @param  : a reference to a positive integer page count
+  @pre    : page count > 0 - books cannot have a negative number of pages
+  @post   : sets the page count to the value of the parameter
+*/
+void Book::setPageCount(const int &page_count){
+  if (page_count > 0)
+  {
+    page_count_ = page_count;
+  }
+}
+
+/**
+  @post   : sets the digital flag to true
+*/
+void Book::setDigital(){
+  is_digital_ = true;
+}
+
+bool Book::operator==(const Book &book)//allows the == operator to be used on book objects to test for equality.
+{
+  if((book.getTitle() == title_) && (book.getAuthor() == author_) && (book.getPageCount() == page_count_) && (book.isDigital() == is_digital_))//tests to see if the object contains the same private variables
+  {
+    return true;//if correct, then they are equal
+  }
+}
+
+bool Book::operator!=(const Book &book)//allows the != operator to be used on book objects to test for inequality.
+{
+  if((book.getTitle() != title_) || (book.getAuthor() != author_) || (book.getPageCount() != page_count_) || (book.isDigital() != is_digital_))//tests to see if any of the private members are differnet
+  {
+    return false;//if they are different, return false
+  }
+}
+
+void Book::display() const
+{
+  std::string digital_true_false = "";//used later in the string sentence
+  if(is_digital_ == true)
+  {
+    digital_true_false = "It is ";//If the book is digital, empty string gets set to "It is"
+  }
+  else
+  {
+    digital_true_false = "It is not ";//If not, it is set to "It is not"
+  }
+  std::cout <<  title_ << " is written by " << author_ << ". " << "Page Count: " << page_count_ << ". " << digital_true_false << "available digitally.\n";
+}
---
+++ LibraryRecord.cpp
@@ -0,0 +1,110 @@
+/*
+Name: Eric Wu
+Professor: Tizana Ligorio
+Description: Creating the LibraryRecord subclass for the project
+Course: CSCI 235 Spring 2023
+Due Date: 03/10/2023
+*/
+
+#include "LibraryRecord.hpp"
+
+bool LibraryRecord::checkIn(const Book &book)
+{
+    bool add_book = add(book);//adds book to items_ using the arraybag add method to see if it is added or not
+    if(add_book)//if book is successfully added, return true
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool LibraryRecord::checkOut(const Book &book)
+{
+    bool remove_book = remove(book);//removes book using arraybag remove method and obtains whether it is removed or not
+    if(remove_book)
+    {
+        return true;
+        checked_out_.push_back(book);//adds the removed book to the list of removed books
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int LibraryRecord::getCheckOutHistory(const Book &book)
+{
+    int counter = 0;
+    for(std::size_t i = 0; i < checked_out_.size(); i++)//loops through list of checked out books
+    {
+        if(checked_out_[i] == book)//if the book param is found within the list, counter gets incremented every time it is found
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+void LibraryRecord::display() const
+{
+    for(std::size_t i = 0; i < checked_out_.size(); i++)
+    {
+        std::cout << checked_out_[i].display() << "\nIt has been checked out " << getCheckOutHistory(checked_out_[i]) << " times.";//uses book method display to get title, author, page count and digital, and then uses getcheckedouthistory to print the number of times a book has been checked out
+    }
+}
+
+void LibraryRecord::displayTitles() const
+{
+    std::string display_titles = "";
+    for(std::size_t i = 0; i < checked_out_.size(); i++)
+    {
+        display_titles + checked_out_[i].getTitle();//adds the name of the title of each book in the checked out list
+        display_titles + ", ";//adds a comma to each title
+    }
+    display_titles + "!";//adds a exclaimation mark once the loop is done to end the string
+    std::cout << display_titles;
+}
+
+bool LibraryRecord::duplicateStock()
+{
+    if(item_count_ * 2 > 10)//if duplicating stock is greater than maximum space, return false
+    {
+        return false;
+    }
+    int starting_item_count = item_count_;
+    for(int i = 0; i < (10 - item_count_); i++)//loop through items
+    {
+            bool add_item = add(items_[i]);
+            if(add_item)
+            {
+            item_count_++;
+            }
+    }
+    if(item_count_ == (2 * starting_item_count))
+    {
+        return true;
+    }
+}
+
+bool LibraryRecord::removeStock(const Book &book)
+{
+    if(contains(book) == false)
+    {
+        return false;
+    }
+    else
+    {
+    for(int i = 0; i < item_count_; i++)
+    {
+        if(items_[i] == book)
+        {
+            remove(items_[i]);
+        }
+    }
+    return true;
+    }
+}
+
