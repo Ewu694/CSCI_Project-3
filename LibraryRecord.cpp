0ff2af23e83fa88f17a08fcd611e4ef670ec86c6
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-03-09 14:06:27 -0500

    Update LibraryRecord.cpp

--- LibraryRecord.cpp
+++ LibraryRecord.cpp
@@ -43,14 +43,14 @@ int LibraryRecord::getCheckOutHistory(const Book &book)
         if(checked_out_[i] == book)//if the book param is found within the list, counter gets incremented every time it is found
         {
             counter++;
-        }
+        }   
     }
     return counter;
 }
 
-void LibraryRecord::display() 
+void LibraryRecord::display()
 {
-    for(std::size_t i = 0; i < checked_out_.size(); i++)
+    for(int i = 0; i < getCurrentSize(); i++)
     {
         checked_out_[i].display();
         std::cout << "\nIt has been checked out " << getCheckOutHistory(checked_out_[i]) << " times.";//uses book method display to get title, author, page count and digital, and then uses getcheckedouthistory to print the number of times a book has been checked out
@@ -69,7 +69,7 @@ void LibraryRecord::displayTitles() const
     std::cout << display_titles;
 }
 
-bool LibraryRecord::duplicateStock()
+bool LibraryRecord::duplicateStock() 
 {
     if(item_count_ * 2 > 10)//if duplicating stock is greater than maximum space, return false
     {
@@ -87,6 +87,10 @@ bool LibraryRecord::duplicateStock()
     if(item_count_ == (2 * starting_item_count))
     {
         return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
@@ -109,11 +113,6 @@ bool LibraryRecord::removeStock(const Book &book)
     }
 }
 
-/**
-    @param:   A reference to another LibraryRecord
-    @return:  Returns true if the 2 library records have the same contents, regardless of their order. For example, if the current holdings of the LibraryRecord are [book1, book2, book3]
-    and those of the referenced LibraryRecord are [book3, book1, book2], it will return true
-    */
 bool LibraryRecord::equivalentRecords(const LibraryRecord &record)
 {
     if(getCurrentSize() != record.getCurrentSize())
@@ -143,49 +142,38 @@ bool LibraryRecord::equivalentRecords(const LibraryRecord &record)
     }
 }
 
-    /**
-        @param:   A reference to another LibraryRecord object
-        @post:    Combines the contents from both LibraryRecord objects, including duplicates.
-        Example: [book1, book2, book3] += [book1, book4] will produce [book1, book2, book3, book1, book4]
-
-        IMPORTANT: We are carrying over the number of times a book has been checked out. For example, if we have LibraryRecord1 += LibraryRecord2 and
-        book4 is in LibraryRecord2 and has been checked out 2 times, then it should still be checked out 2 times in LibraryRecord1 after the += operation
-        Hint: use getCheckOutHistory and the checkout history vector
-    */
-void LibraryRecord::operator+=(const LibraryRecord &record)
+void LibraryRecord::operator+=(LibraryRecord &record)
 {
-    for(size_t i = 0; i < record.getCurrentSize(); i++)
+    for(int i = 0; i < record.getCurrentSize(); i++)
     {
-        if(item_count_ < 10)
+        if(item_count_ == DEFAULT_CAPACITY)
         {
-        add(record.items_[i]);
+            return;
         }
-    }
-    int times_appeared = 0;
-    int num_times_checked_out = 0;
-    for(int i = 0; i < item_count_; i++)
-    {
-        int index_book_location = getIndexOf(record.items_[i]);
-        times_appeared = getFrequencyOf(record.items_[i]);
-        num_times_checked_out = getCheckOutHistory(checked_out_[index_book_location]);
+        add(record.items_[i]);
+        //int history = ;
+        for(int j = 0; j < record.getCheckOutHistory(record.items_[i]); j++)
+        {
+            checked_out_.push_back(record.items_[i]);
+        }
     }
 }
 
-    /** @param:   A reference to another LibraryRecord object
-        @post:    Combines the contents from both LibraryRecord objects, EXCLUDING duplicates.
-        Example: [book1, book2, book3] /= [book1, book4] will produce [book1, book2, book3, book4]
-
-        IMPORTANT: We are carrying over the nunber of times a book has been checked out. For example, if we have LibraryRecord1 /= LibraryRecord2 and
-        book4 is in LibraryRecord2 and has been checked out 2 times, then it should still be checked out 2 times in LibraryRecord1 after the /= operation
-        Hint: use getCheckOutHistory and the checkout history vector
-    */
-void LibraryRecord::operator/=(const LibraryRecord &record)
+void LibraryRecord::operator/=(LibraryRecord &record)
 {
-    for(size_t i = 0; i < record.getCurrentSize(); i++)
+    for(int i = 0; i < record.getCurrentSize(); i++)
     {
-        if(item_count_ < 10 && !contains(record.items_[i]))
+        if(item_count_ == DEFAULT_CAPACITY)
         {
-        add(record.items_[i]);
+            return;
+        }
+        else if(!contains(record.items_[i]))
+        {
+            add(record.items_[i]);
+        }
+        for(int j = 0; j < record.getCheckOutHistory(record.items_[i]); j++)
+        {
+            checked_out_.push_back(record.items_[i]);
         }
     }
 }
