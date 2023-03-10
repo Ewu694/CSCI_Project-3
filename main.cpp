2ec008d36fe2cb63bd211affae7ccf41a6314333
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-03-10 10:21:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,37 +3,12 @@
 #include "LibraryRecord.hpp"
 
 int main() {
-    std::cout << "TEST 1: TEST DEFAULT CONSTRUCTOR & ACCESSORS" << std::endl;
-
-    // Test default constructor
     Book my_book;
-
-    // Test accessor functions
-    std::cout << my_book.getTitle() << std::endl;
-    std::cout << my_book.getAuthor() << std::endl;
-    std::cout << my_book.getPageCount() << std::endl;
-    std::cout << my_book.isDigital() << std::endl;
-
-
-    std::cout << "\nTEST 2: TEST PARAMETERIZED CONSTRUCTOR & ACCESSORS" << std::endl;
 
     // Test parameterized constructor
     Book my_book1("Harry Potter and the Chamber of Secrets", "J.K Rowling", 253, 1);
     Book my_book2("Moby Dick", "Herman Melville", 378);
-    
-    // Test accessor functions
-    std::cout << my_book1.getTitle() << " By " << my_book1.getAuthor() 
-        << " with page count " << my_book1.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book1.isDigital() << std::endl;
 
-    std::cout << my_book2.getTitle() << " By " << my_book2.getAuthor() 
-        << " with page count " << my_book2.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book2.isDigital() << std::endl;
-
-    
-    std::cout << "\nTEST 3: TEST MUTATOR FUNCTIONS" << std::endl;
-
-    // Test mutator functions
     Book my_book3("Moby Dick", "Herman Melville", 378);
     my_book3.setTitle("50 shades of gray");
     my_book3.setAuthor("E. L. James");
@@ -41,24 +16,14 @@ int main() {
     my_book3.setPageCount(-10);
     my_book3.setDigital();
 
-    // Test accessor functions
-    std::cout << my_book3.getTitle() << " By " << my_book3.getAuthor() 
-        << " with page count " << my_book3.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book3.isDigital() << std::endl;
-
-    if(my_book1 == my_book2)
-    {
-        std::cout << "Books are the same\n";
-    }
-    else
-    {
-        std::cout << "Book are different\n";
-    }
-    my_book1.display();
     LibraryRecord record1;
-    std::cout << record1.checkIn(my_book1);
+    record1.checkIn(my_book1);
     record1.checkIn(my_book2);
     record1.checkIn(my_book3);
-    std::cout << record1.checkOut(my_book1);
-    std::cout << record1.getCheckOutHistory(my_book1);
+    record1.checkOut(my_book1);
+    record1.checkIn(my_book1);
+    record1.checkOut(my_book1);
+    record1.removeStock(my_book2);
+    std::cout << record1.duplicateStock() << "\n" << std::endl;
+    record1.display();
 }
